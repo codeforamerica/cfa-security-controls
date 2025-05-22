e286667a3ce6ff774ba252acd0082a9610ba1c8f
James Armes <jarmes@codeforamerica.org>
2025-05-22 17:50:29 -0400

    feat: Support username and password for Aptible.

--- hyperproof/lib/cfa_security_controls/hyperproof/clients/aptible.rb
+++ hyperproof/lib/cfa_security_controls/hyperproof/clients/aptible.rb
@@ -7,6 +7,8 @@ module CfaSecurityControls
     module Clients
       # Client for Aptible.
       class Aptible
+        class InvalidCredentials < ArgumentError; end
+
         TOKEN_FILE = File.join(Dir.home, '.aptible', 'tokens.json')
 
         # Retrieve all databases from Aptible.
@@ -20,11 +22,50 @@ module CfaSecurityControls
 
         # Retrieve a token to authenticate with Aptible.
         #
+        # This method uses a chain to find the first valid set of credentials:
+        #
+        # 1. Attempt to use `APTIBLE_USERNAME` and `APTIBLE_PASSWORD`
+        #    environment variables.
+        # 2. Check for the Aptible SSO token file at `~/.aptible/tokens.json`.
+        #
+        # If no valid credentials are found, an error is raised.
+        #
         # @return [String] The token for the Aptible API.
         #
-        # @todo Support running in CI where we don't have an SSO token file.
+        # @raise [InvalidCredentials] If no valid credentials are found.
         def token
-          @token ||= JSON.parse(File.read(TOKEN_FILE))[::Aptible::Auth.configuration.root_url]
+          @token ||= basic_auth_token || sso_token
+
+          raise InvalidCredentials, 'No valid Aptible credentials found' unless @token
+        end
+
+        # Retrieve the SSO token from the Aptible token file.
+        #
+        # @return [String, Boolean] The SSO token for the Aptible API, or false
+        #   if the token isn't set.
+        def sso_token
+          return false unless File.exist?(TOKEN_FILE)
+
+          contents = JSON.parse(File.read(TOKEN_FILE))
+          return false unless contents.key?(
+            ::Aptible::Auth.configuration.root_url
+          )
+
+          contents[::Aptible::Auth.configuration.root_url]
+        end
+
+        # Retrieve a basic auth token to authenticate with Aptible.
+        #
+        # @return [Aptible::Auth::Token, Boolean] The token for the Aptible API,
+        #   or false if the credentials aren't present.
+        def basic_auth_token
+          email = ENV.fetch('APTIBLE_USERNAME', nil)
+          password = ENV.fetch('APTIBLE_PASSWORD', nil)
+          return false unless email && password
+
+          ::Aptible::Auth::Token.create(email:, password:)
+        rescue OAuth2::Error
+          false
         end
       end
     end
